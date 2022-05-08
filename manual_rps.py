18c7061458db89e1a1aa4ea4208950455a23df7e
Piotr Z. Jelonek <piotr.z.jelonek@gmail.com>
2022-05-08 08:47:26 +0100

    Find out who won

--- manual_rps.py
+++ manual_rps.py
@@ -1,30 +1,69 @@
 import numpy as np
 from random import choice
 
+def get_computer_choice(actions: list) -> str:
 
+    action = choice(actions)
+    print(f"Computer action: {action}")
 
-def get_computer_choice(actions: list):
-    return choice(actions)
+    return action
 
-def get_player_choice(actions: list):
+def get_player_choice(actions: list) -> str:
+
     action = ''
     while action not in actions:
         action = input("Choose your action: ").lower()
         action = action[0].upper() + action[1:]
+
+    if action == 'Nothing': print("Hey! Choose your action.\n")
+
     return action
 
+def get_winner(player_action: str, computer_action: str) -> int:
 
-############
+    if computer_action in lost_against[player_action]:
 
-print("All hail Sam Kass! Hail!")
+        print("\nThis time you win")
+        won = 1
 
-actions = ['Rock','Paper','Sciccors','Lizard','Spock']
+    elif player_action in lost_against[computer_action]:
 
-computer_action = get_computer_choice(actions)
+        print("\nThis time computer won")
+        won = -1
 
-print(computer_action)
+    else: 
 
-player_action = get_player_choice(actions)
-print(player_action)
+        print("\nIt is a draw!")
+        won = 0
+    
+    print("")
 
-print("\n")
+    return won
+
+
+actions = ['Rock','Paper','Scissors','Lizard','Spock']
+lost_against = {
+    'Rock': ['Lizard', 'Scissors'], 
+    'Paper': ['Rock','Spock'], 
+    'Scissors': ['Paper','Lizard'], 
+    'Spock': ['Scissors','Rock'], 
+    'Lizard': ['Spock', 'Paper'] 
+    }
+player_win_count = 0
+computer_win_count = 0
+
+print("\nAll hail Sam Kass! Hail!\n")
+
+player_action = get_player_choice(actions + ['Nothing'])
+
+if player_action != 'Nothing':
+
+    computer_action = get_computer_choice(actions)
+    won = get_winner(player_action, computer_action)
+
+    if won == 1: 
+        player_win_count += 1
+    elif won == -1:
+        computer_win_count +=1
+
+game_continues = (player_win_count < 3 and computer_win_count < 3)
